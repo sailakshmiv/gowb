4675e9b412fdb85a233c197e741d35401be1e3b8
sailakshmiv <lakshmi.saiv99@gmail.com>
2016-09-20 16:33:15 +0530

    Update gowb.go

--- gowb.go
+++ gowb.go
@@ -1,13 +1,35 @@
-/*
-Author : Akshay Deo
-Email  : akshay@betacraft.co
-*/
 package main
 
 import (
-	"log"
+	"net/http"
+	"text/template"
 )
 
-func main() {
-	log.Println("Okay")
+func main()  {
+	http.HandleFunc("/",func(w http.ResponseWriter,req *http.Request){
+		w.Header().Add("Cotent type", "text/html")
+		tmpl,err:= template.New("test").Parse(doc)
+		if err == nil{
+			context:= Context{Message:"Sample web App",Title:"Title of Hello world"}
+			tmpl.Execute(w,context)
+		}
+	})
+	http.ListenAndServe(":8080",nil)
+}
+
+const doc = `
+<!DOCTYPE html>
+<html>
+<head>
+<title> {{.Title}}</title>
+</head>
+<body>
+<h1> {{.Message}}</h1>
+</body>
+</html>
+`
+
+type Context struct {
+	Title string
+	Message string
 }
